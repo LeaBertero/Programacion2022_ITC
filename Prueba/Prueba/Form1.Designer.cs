a978b3a66db1ff4046195be40211c7cc17d6f382
Leandro Bertero <leabertero@hotmail.com>
2022-05-29 11:59:29 -0300

    programa de proeba de checkbox

--- Prueba/Prueba/Form1.Designer.cs
+++ Prueba/Prueba/Form1.Designer.cs
@@ -28,13 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Btseleccion = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // Btseleccion
+            // 
+            this.Btseleccion.Location = new System.Drawing.Point(104, 104);
+            this.Btseleccion.Name = "Btseleccion";
+            this.Btseleccion.Size = new System.Drawing.Size(75, 23);
+            this.Btseleccion.TabIndex = 0;
+            this.Btseleccion.Text = "button1";
+            this.Btseleccion.UseVisualStyleBackColor = true;
+            this.Btseleccion.Click += new System.EventHandler(this.Btseleccion_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(235, 110);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(235, 152);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(235, 200);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(235, 242);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.Btseleccion);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Btseleccion;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
     }
 }
 
--- Prueba/Prueba/Form1.cs
+++ Prueba/Prueba/Form1.cs
@@ -16,5 +16,30 @@ namespace Prueba
         {
             InitializeComponent();
         }
+
+        private void Btseleccion_Click(object sender, EventArgs e)
+        {
+            //if (checkBox1.Checked || checkBox2.Checked || checkBox3.Checked || checkBox4.Checked == false)
+            //{
+            //    MessageBox.Show("Ingrese una de las opciones");
+            //}
+
+            if (checkBox1.Checked && checkBox2.Checked == true)
+            {
+                MessageBox.Show("Se han marcado las casillas 1 y 2 solamente");
+            }
+            if (checkBox1.Checked == false && checkBox2.Checked && checkBox3.Checked && checkBox4.Checked == true)
+            {
+                MessageBox.Show("Se han marcado casillas 2,3 y 4 , menos la casilla 1");
+            }
+            if (checkBox1.Checked && checkBox2.Checked && checkBox3.Checked && checkBox4.Checked == false)
+            {
+                MessageBox.Show("No se ha marcado ninguna casilla");
+            }
+            //if (checkBox1.Checked == false && checkBox2.Checked && checkBox3.Checked && checkBox4.Checked == true)
+            //{
+            //    MessageBox.Show("Se han marcado casillas 2,3 y 4 , menos la casilla 1");
+            //}
+        }
     }
 }
