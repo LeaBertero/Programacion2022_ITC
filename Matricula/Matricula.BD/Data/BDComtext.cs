a968f5681bf4369c151ee26e6c15f295aa352838
Leandro Bertero <leabertero@hotmail.com>
2022-05-30 23:32:19 -0300

    trabajando con BDContext : DbContext

--- Matricula/Matricula.BD/Data/BDComtext.cs
+++ Matricula/Matricula.BD/Data/BDComtext.cs
@@ -4,13 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Matricula.BD.Data.Entidades;
 
 namespace Matricula.BD.Data
 {
+
+    //Representacion de la Base de datos
     public class BDComtext : DbContext
     {
+        public DbSet<Medico> Medicos { get; set; }
 
+        public BDComtext(DbContextOptions options) : base(options)
+        {
+
+        }
     }
 
-    
+
 }
---
+++ Matricula/Matricula.BD/Data/Entidades/Medico.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matricula.BD.Data.Entidades
+{
+    public class Medico
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
