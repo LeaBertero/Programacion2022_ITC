8858c77d877723ce0d9f5172d52fd320fa4e6ba5
Leandro Bertero <leabertero@hotmail.com>
2022-06-13 12:56:34 -0300

    Segundo Comit (Carga de productos_orden en el cogo, separando por Atributos_constructores_metodos)

--- WinFormProducto1/Entidades/Producto.cs
+++ WinFormProducto1/Entidades/Producto.cs
@@ -8,5 +8,41 @@ namespace Entidades
 {
     internal class Producto
     {
+        #region Atributos
+        private int codigo;
+        private string Descripcion;
+        private int Stock;
+        #endregion
+
+        #region Constructores
+        public Producto()
+        {
+
+        }
+
+        public Producto(int Cod, string Desc)
+        {
+            codigo = Cod;
+            Descripcion = Desc; 
+            Stock = 0;
+        }
+        #endregion
+
+        #region Metodos
+
+        public void Imgreso(int Cantidad)
+        {
+            Stock = Stock + Cantidad;
+        }
+
+        public void Salida(int Cant)
+        {
+            Stock = Stock - Cant;
+        }
+        #endregion
     }
 }
+
+
+
+     
